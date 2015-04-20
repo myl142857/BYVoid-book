e727de4cc12d6a9399a3a669f28463d00550b2c3
ma anrui <zhudy@yahoo.com>
2015-04-20 15:54:29 +0800

    4.4.3 fs.open, fs.read

--- readfile.js
+++ readfile.js
@@ -2,7 +2,8 @@
 
 var fs=require('fs');
 
-fs.readFile('file.txt', 'utf-8', function(err, data){
+//fs.readFile('file.txt', 'utf-8', function(err, data){
+fs.readFile('file.txt', function(err, data){
     if(err){
 	console.error(err);
     }else{
