d378be2b0ecfed766f869378f818ff23f3ded358
ma anrui <zhudy@yahoo.com>
2015-04-20 15:38:36 +0800

    4.3.1

--- event.js
+++ event.js
@@ -3,10 +3,14 @@
 var EventEmitter = require('events').EventEmitter;
 var event = new EventEmitter();
 
-event.on('some_event', function(){
-    console.log('seome_event occured.');
+event.on('some_event', function(argv1, argv2){
+    console.log('listener1', argv1, argv2);
+});
+
+event.on('some_event', function(argv1, argv2){
+    console.log('listener2', argv1, argv2);
 });
 
 setTimeout(function(){
-    event.emit('some_event');
+    event.emit('some_event', 'byvoid', 1991);
 }, 1000);
