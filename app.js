8926d0ae28d9c2b17a0db745ed41c3b23ab2f3a4
ma anrui <zhudy@yahoo.com>
2015-04-20 16:10:35 +0800

    4.5.1 http server

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 //app.js
 
 var http=require('http');
+var util = require('util');
+var url = require('url');
 
 http.createServer(function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<h1>Node.js</h1>');
-  res.end('<p>Hello World</p>');
+  res.end('<p>Hello World</p>' + util.inspect(url.parse(req.url, true)));
 }).listen(80);
 console.log('HTTP server is listening at port 80.');
 
